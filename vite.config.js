c5b4c56970c5c26f5ba6089a15f91bb526496d0b
Emo <emanuilgerganov@gmail.com>
2022-07-27 16:43:36 +0300

    add pages plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
+
+// plugins
 import vue from "@vitejs/plugin-vue";
+import Pages from "vite-plugin-pages";
+// import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  // plugins: [vue(), Pages(), Components()],
+  plugins: [vue(), Pages()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
