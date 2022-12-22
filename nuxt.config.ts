a82941ddae82c52938152c8398468eac92d83bc9
Emo <emanuilgerganov@gmail.com>
2022-12-22 13:11:51 +0200

    add provider:static to nuxt image buildModule

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@vueuse/nuxt", "@nuxt/image-edge"],
+  modules: ["@vueuse/nuxt", ["@nuxt/image-edge", { provider: "static" }]],
   ssr: false,
   css: ["~/assets/main.css"],
   postcss: {
